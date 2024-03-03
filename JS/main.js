251f60801512679f314b675583593612d263985f
mk-munna <mkmunnaofficial@gmail.com>
2024-03-03 19:37:16 +0600

    defauld post show, mark read div color change, when click mail open icon show doooone!

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,71 @@
 
 
+const getData = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await response.json();
+    const posts = data.posts;
+    displayData(posts);
+    // console.log(data.posts);
+};
+loaderOn()
+getData();
+const displayData = (posts) => {
+    const postDivContainer = document.getElementById('post-div-container');
+    const post = posts.forEach(post => {
+        // console.log(post);
+        if (post.isActive) {
+            const active = 'bg-green-700 '
+            createPost(active, post);
+        } else {
+            const inactive = 'bg-red-200 '
+            createPost(inactive, post);
+        }
+        function createPost(isActive, post) {
+            const postDiv = document.createElement('div');
+            postDiv.setAttribute('class', 'post-div p-4 lg:p-10 bg-[#797DFC1A] rounded-3xl');
+            postDiv.innerHTML = `
+        <div class="flex gap-6">
+                            <div class="w-[70px] h-[70px] relative">
+                                <div class="w-[12px] ${isActive} h-[12px] rounded-full absolute -right-1 -top-1"></div>
+                                <img class="rounded-md"
+                                    src="${post?.image || 'not found 😌'}"
+                                    alt="">
+                            </div>
+                            <div class="text-sm"><span>#${post?.category}</span>
+                                <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Author : ${post?.author?.name || 'not found  😌'}</span>
+                            </div>
 
+                        </div>
+                        <div class=" text-base font-semibold text-[#12132DCC] lg:ml-[90px] lg:-mt-[40px] space-y-3">
 
-
-
-
-
-
-const icon = document.getElementsByClassName('tp');
-
-for (let i = 0; i < icon.length; i++) {
-    icon[i].addEventListener('mouseover', function (event) {
-        this.style.transform = "scale(1.1)";
-        this.children[0].style.visibility = 'visible';
-        // console.log(this.children[0]);
+                            <h1 class="text-xl post-title font-bold">${post?.title || 'not found 😌'}</h1>
+                            <p>${post?.description || 'not found 😌'}</p>
+                            <hr>
+                            <div class="flex justify-between"><!--last icon container-->
+                                <div class="gap-6 lg:gap-12 text-gray-500 flex">
+                                    <p><i class="fa-regular fa-comment-dots"></i> <span> &nbsp; ${post?.comment_count || 'not found 😌'}</span></p>
+                                    <p><i class="fa-regular fa-eye"></i> <span> &nbsp; ${post?.view_count || 'not found 😌'}</span></p>
+                                    <p><i class="fa-regular fa-clock"></i> <span> &nbsp; ${post?.posted_time || 'not found 😌'}</span></p>
+                                </div>
+                    <div class="bg-green-500 text-white px-2 py-1 rounded-full"><button class="read" ><i class="fa-solid fa-envelope"></i></button></div>
+                            </div>
+                        </div>`;
+            // console.log(postDiv);
+            // console.log(post);
+            setTimeout(() => {
+                loaderOff();
+                postDivContainer.appendChild(postDiv);
+                markRead();
+            },2000);  
+        }
     });
-    icon[i].addEventListener('mouseout', function () {
-        this.style.transform = "scale(1)";
-        this.children[0].style.visibility = 'hidden';
-        
-    });
+}
+
+// loader
+function loaderOn() {
+    document.getElementById('loader').classList.remove('hidden');
+}
+
+function loaderOff() {
+    document.getElementById('loader').classList.add('hidden');
 }
---
+++ JS/utilities.js
@@ -0,0 +1,68 @@
+
+// icon tooltip and hover effects
+const icon = document.getElementsByClassName('tp');
+
+for (let i = 0; i < icon.length; i++) {
+    icon[i].addEventListener('mouseover', function () {
+        this.style.transform = 'scale(1.1)';
+        this.style.transitionDuration = '0.1s';
+        this.children[0].style.visibility = 'visible';
+        // console.log(this.children[0]);
+    });
+    icon[i].addEventListener('mouseout', function () {
+        this.style.transform = 'scale(1)';
+        this.style.transitionDuration = '0.1s';
+        this.children[0].style.visibility = 'hidden';
+
+    });
+}
+//
+// input search scale
+document.getElementById('search-input').addEventListener('focus', function () {
+    this.style.transform = 'scale(1.1)';
+    this.style.transitionDuration = '0.2s';
+});
+document.getElementById('search-input').addEventListener('blur', function () {
+    this.style.transform = 'scale(1)';
+    this.style.transitionDuration = '0.2s';
+});
+
+
+const markRead = ()=>{
+    let markCount = 0;
+    const postDiv = document.querySelectorAll('.post-div');
+    const post = postDiv.forEach((post) => {
+        const readBtn = post.children[1].childNodes[7].childNodes[4].childNodes[0].childNodes[0];
+        readBtn.addEventListener('mouseover', function () {
+            this.style.transform = 'scale(1.05)';
+            this.style.transitionDuration = '0.1s';
+        });
+        readBtn.addEventListener('mouseout', function () {
+            this.style.transform = 'scale(1)';
+            this.style.transitionDuration = '0.1s';
+        });
+
+        readBtn.onclick = function () {
+            markCount++;
+            document.getElementById('mark-count').innerText = markCount;
+            const viewCount = this.parentNode.parentNode.previousSibling.previousSibling.childNodes[3].innerText;
+            // console.log(viewCount);
+            const postTitle = this.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText;
+            // console.log(postTitle);
+            readBtn.parentElement.innerHTML = `<i class="fa-solid fa-envelope-open"></i>`
+            post.classList.remove('bg-[#797DFC1A]');
+            post.classList.add('bg-gray-100');
+
+            const markedDiv = document.getElementById('marked-div');
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'flex gap-4 bg-gray-50 rounded-xl p-3');
+            newDiv.innerHTML = `
+            <p class="text-base font-semibold">${postTitle}</p>
+                            <div class="w-[150px]">
+                                <p><i class="fa-regular fa-eye"></i> <span> &nbsp; ${viewCount}</span></p>
+                            </div>
+            `;
+            markedDiv.appendChild(newDiv);
+        }
+    })
+}
